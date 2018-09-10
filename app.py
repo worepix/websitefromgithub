701f005bb4a9b6eeccf591b4b7d8cc16f6032c24
worepix <hry-kurasa@seznam.cz>
2018-09-10 22:56:41 +0200

    Fix

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def cli(run, repository, install):
             os.makedirs(settings_location)
             os.system("cd " + settings_location)
             settings_file = open("settings.json")
-            settings_file.write(requests.get("https://raw.githubusercontent.com/worepix/websitefromgithub/settings.json").text)
+            settings_file.write(requests.get("https://raw.githubusercontent.com/worepix/websitefromgithub/master/settings.json").text)
 
         set_repository = input("Paste your GitHub repository. For example: https://github.com/worepix/websitefromgithub\n")
         open(settings_location + "settings.json", "w").write('''{\n    "GitHub": "'''+ set_repository + '''"\n}''')
