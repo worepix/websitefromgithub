f533441e14f4f5701d1c6a9dacd7ebe34860dedc
worepix <hry-kurasa@seznam.cz>
2018-10-02 22:00:47 +0200

    Update: Getting variables, counter

--- websitefromgithub.js
+++ websitefromgithub.js
@@ -2,19 +2,32 @@ const md5File = require('md5-file');
 var delay = require("delay");
 var fs = require('fs');
 const { execSync } = require('child_process');
-
-var checkper = 1000;                // Checking changes frequency in milliseconds
 var settingsfile = "settings.json"; // File where settings is located
 
 var settings_GitHub_url;
 var settings_GitHub_name;
 var settings_web_location;
 var settings_command;
+var settings_frequency;
 
 getvariables();
-counter();
 
-// Getting variables from settings.json
+// Check for changes
+function check() {
+    console.log(settings_command);
+}
+
+
+// Frequency counter
+function counter() {
+(async () => {
+    check();
+    await delay(settings_frequency);
+    counter();
+})();
+}
+    
+// Getting variables from settings.json and run function counter
 function getvariables(){
 
     fs.readFile(settingsfile, (err, data) => {
@@ -28,20 +41,8 @@ function getvariables(){
         settings_GitHub_name = settings_GitHub_url.split("/")[4].substring(0, ((settings_GitHub_url.split("/")[4]).toString()).length-4);
         settings_web_location = JSON.parse(data.toString()).web_location;
         settings_command = JSON.parse(data.toString()).command;
+        settings_frequency = parseInt((JSON.parse(data.toString()).frequency).toString());
+        counter();
         }
     });
-}
-
-// Frequency counter
-function counter() {
-(async () => {
-    await delay(checkper);
-    check();
-    counter();
-});
-}
-
-// Check for changes
-function check() {
-    
 }
